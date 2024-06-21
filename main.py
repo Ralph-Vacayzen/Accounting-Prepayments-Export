ce80161cdc9bd95cfbdae127fa33a21a8823230a
Ralph McCracken <ralph@mccracken.work>
2024-06-21 11:57:06 -0500

    overhaul

--- main.py
+++ main.py
@@ -16,9 +16,7 @@ with st.expander('Uploaded Files'):
         ['Partners.csv','Google Drive > Shared with me > Tech'],
         ['Site_To_Partner.csv','Google Drive > Shared with me > Tech'],
         ['House_Agreements.csv','Google Drive > Shared with me > Tech'],
-        ['CTLL_RentalAgreementLines.csv','Google Drive > Shared with me > Tech'],
         ['CTLL_RentalAgreement.csv','Google Drive > Shared with me > Tech'],
-        ['CTLL_PartnerSiteRentalAgreements.csv','Google Drive > Shared with me > Tech'],
         ['Prepayment Export.csv','Google Drive > Shared with me > Tech']
     ]
 
@@ -26,9 +24,7 @@ with st.expander('Uploaded Files'):
         'Partners.csv': None,
         'Site_To_Partner.csv': None,
         'House_Agreements.csv': None,
-        'CTLL_RentalAgreementLines.csv': None,
         'CTLL_RentalAgreement.csv': None,
-        'CTLL_PartnerSiteRentalAgreements.csv': None,
         'Prepayment Export.csv': None
     }
 
@@ -71,72 +67,38 @@ elif len(uploaded_files) > 0 and hasAllRequiredFiles:
     partners               = pd.read_csv(uploaded_files[files['Partners.csv']])
     sites                  = pd.read_csv(uploaded_files[files['Site_To_Partner.csv']])
     houseAgreements        = pd.read_csv(uploaded_files[files['House_Agreements.csv']])
-    partnerSiteAgreements  = pd.read_csv(uploaded_files[files['CTLL_PartnerSiteRentalAgreements.csv']])
-    ral                    = pd.read_csv(uploaded_files[files['CTLL_RentalAgreementLines.csv']])
     ra                     = pd.read_csv(uploaded_files[files['CTLL_RentalAgreement.csv']])
     pp                     = pd.read_csv(uploaded_files[files['Prepayment Export.csv']])
 
 
-#################################################
-#
-# Revenue KPI
-#
-#################################################
+    src = pd.merge(partners,        sites, how='left')
+    ha  = pd.merge(houseAgreements, ra,    how='left', left_on='RentalAgreementID', right_on='ID')
+
+    df  = pd.merge(pp, ra,  how='left', left_on='RentalAgreementNo', right_on='ID', validate='m:1')
+    src_temp = src.drop_duplicates(subset='CID')
+    df  = pd.merge(df, src_temp, how='left', left_on='CustomerNumber',    right_on='CID')
+    df  = df.drop(columns=['OriginSource_y'])
+    df  = df.rename(columns={'OriginSource_x': 'OriginSource', 'Customer': 'PartnerOrder'})
+    df  = pd.merge(df, src, how='left', on='OriginSource')
+    df  = df.drop(columns=['CID_x','CID_y'], axis=1)
+    df  = df.rename(columns={'Customer': 'PartnerSiteOrder'})
+    ha_temp = ha.drop_duplicates(subset=['AgrmtJobAddrLat','AgrmtJobAddrLong'], keep='last')
+    df  = pd.merge(df, ha_temp,  how='left', on=['AgrmtJobAddrLat','AgrmtJobAddrLong'], validate='m:1')
+    df  = df.drop(columns=['ID_x','OriginSource_x','RentalAgreementID','ID_y','OriginSource_y','CustomerNumber_y','AgrmtJobAddrLong','CustomerNumber_x'])
+    df  = df.rename(columns={'Partner': 'PartnerLatLong'})
+
+    def Attribute_Prepayment_To_Partner(row):
+        if pd.notna(row.PartnerOrder):
+            return row.PartnerOrder
+        
+        if pd.notna(row.PartnerSiteOrder):
+            return row.PartnerSiteOrder
+        
+        if row.AgrmtJobAddrLat != 0 and row.AgrmtJobAddrLat and pd.notna(row.PartnerLatLong):
+            return row.PartnerLatLong
 
 
-    # Define House Agreement Versus Rental Agreement
+    df['Customer'] = df.apply(Attribute_Prepayment_To_Partner, axis=1)
+    df = df.drop(columns=['AgrmtJobAddrLat','PartnerOrder','PartnerSiteOrder','PartnerLatLong'], axis=1)
 
-    houseAgreements.sort_values(by=['RentalAgreementID'])
-
-    house  = ral[ral['RentalAgreementID'].isin(houseAgreements.RentalAgreementID.array)]
-    rental = ral[~ral['RentalAgreementID'].isin(houseAgreements.RentalAgreementID.array)]
-
-
-    # House Agreement (Lat, Long) => Partner
-
-    df           = house[['CustomerNumber','Latitude','Longitude']]
-    df           = df[df['CustomerNumber'].isin(partners.CID.array)]
-    df           = df.drop_duplicates()
-    ra2c         = pd.merge(df, ra, left_on=['Latitude','Longitude'], right_on=['AgrmtJobAddrLat', 'AgrmtJobAddrLong'], how="left")
-    ra2c         = ra2c.drop_duplicates(keep="last")
-    ra2c         = ra2c[ra2c != 0]
-    ra2c         = ra2c.dropna(subset=['AgrmtJobAddrLat','AgrmtJobAddrLong'])
-    ra2c         = ra2c[['ID','CustomerNumber']]
-    ra2c.columns = ['RentalAgreementNo','Partner']
-
-
-    # Partner Site Submission Rental Agreement (Lat, Long) => Partner
-
-    siteToPartner      = pd.merge(partners, sites, on="CID")
-    partnerSiteLatLong = pd.merge(partnerSiteAgreements, ra, on="ID", how="left")
-    partnerToLatLong   = pd.merge(partnerSiteLatLong, siteToPartner, on="OriginSource", how="left")
-    partnerToLatLong   = partnerToLatLong[['CID','AgrmtJobAddrLat','AgrmtJobAddrLong']]
-    partnerToLatLong   = partnerToLatLong.drop_duplicates(keep="last")
-    ra2p               = pd.merge(partnerToLatLong, ra, left_on=['AgrmtJobAddrLat', 'AgrmtJobAddrLong'], right_on=['AgrmtJobAddrLat', 'AgrmtJobAddrLong'], how="left")
-    ra2p               = ra2p.drop_duplicates(keep="last")
-    ra2p               = ra2p[ra2p != 0]
-    ra2p               = ra2p.dropna(subset=['AgrmtJobAddrLat','AgrmtJobAddrLong'])
-    ra2p               = ra2p[['ID','CID']]
-    ra2p.columns       = ['RentalAgreementNo','Partner']
-
-
-    # Combine (House Agreement (Lat, Long) => Partner) & (Partner Site Submission Rental Agreement (Lat, Long) => Partner)
-
-    ra2p = pd.concat([ra2c, ra2p])
-    ra2p.drop_duplicates()
-
-    # Generate Results
-
-    result          = pd.merge(pp, ra2p, how='left')
-    result          = result.drop_duplicates(subset=['RentalAgreementNo','RentalAgreementStartDate','RentalAgreementEndDate','RentalAgreementReservationStartDate','RentalAgreementReservationEndDate','TransactionAmount','PaymentDate','PaymentMethod'],keep='last')
-    result          = pd.merge(result, partners, left_on="Partner", right_on="CID", how="left")
-
-    result['RentalAgreementStartDate']            = pd.to_datetime(result['RentalAgreementStartDate']).dt.normalize()
-    result['RentalAgreementEndDate']              = pd.to_datetime(result['RentalAgreementEndDate']).dt.normalize()
-    result['RentalAgreementReservationStartDate'] = pd.to_datetime(result['RentalAgreementReservationStartDate']).dt.normalize()
-    result['RentalAgreementReservationEndDate']   = pd.to_datetime(result['RentalAgreementReservationEndDate']).dt.normalize()
-    result['PaymentDate']                         = pd.to_datetime(result['PaymentDate']).dt.normalize()
-
-    result = result.drop(columns=['Partner','CID'])
-
-    st.download_button(label='DOWNLOAD', data=result.to_csv(index=False), file_name='prepayments.csv', mime='csv', use_container_width=True, type='primary')
+    st.download_button(label='DOWNLOAD', data=df.to_csv(index=False), file_name='prepayments.csv', mime='csv', use_container_width=True, type='primary')
